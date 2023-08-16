aaf644296c646526b1aa2c5f79cb82701b67b22d
matan-h <matan.honig2@gmail.com>
2023-08-16 21:10:08 +0300

    a first non-working interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // Import your CSS file
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+enum Screen {
+  UPLOAD,
+  PREVIEW,
+  CONVERTING,
+  CONVERTED
 }
+
+const App: React.FC = () => {
+  const [currentScreen, setCurrentScreen] = useState<Screen>(Screen.UPLOAD);
+  const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
+  const [convertedFile, setConvertedFile] = useState<File | null>(null);
+  const [conversionProgress, setConversionProgress] = useState<number>(0);
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      setSelectedFiles(files);
+      setCurrentScreen(Screen.PREVIEW);
+      handleConvert();
+    }
+  };
+
+  const handleConvert = () => {
+    setCurrentScreen(Screen.CONVERTING);
+    simulateConversion(); // Placeholder for actual conversion logic
+  };
+
+  const simulateConversion = () => {
+    // Simulate conversion progress
+    let progress = 0;
+    const interval = setInterval(() => {
+      progress += 10;
+      setConversionProgress(progress);
+      if (progress >= 100) {
+        clearInterval(interval);
+        setCurrentScreen(Screen.CONVERTED);
+        setConvertedFile(selectedFiles?.[0] || null);
+      }
+    }, 500);
+  };
+
+  const handleReset = () => {
+    setSelectedFiles(null);
+    setConvertedFile(null);
+    setConversionProgress(0);
+    setCurrentScreen(Screen.UPLOAD);
+  };
+
+  const renderScreen = () => {
+    switch (currentScreen) {
+      case Screen.UPLOAD:
+        return (
+          <div className="full-screen">
+            <div className="upload-area">
+              <p className="upload-text">Drag files here or click here to upload files</p>
+              <input type="file" multiple onChange={handleFileUpload} />
+            </div>
+          </div>
+        );
+
+      case Screen.PREVIEW:
+        return (
+          <div className="content">
+            {/* Display preview of uploaded files */}
+            <select className="dropdown">
+              {/* Dropdown for "convert to" options */}
+            </select>
+            <button className="action-button" onClick={handleReset}>Reset</button>
+            <button className="action-button" onClick={handleConvert}>Convert</button>
+          </div>
+        );
+
+      case Screen.CONVERTING:
+        return (
+          <div className="content">
+            <p>Converting...</p>
+            <progress className="progress-bar" value={conversionProgress} max={100} />
+          </div>
+        );
+
+      case Screen.CONVERTED:
+        return (
+          <div className="content">
+            {/* Display preview of the converted file */}
+            <button className="action-button">Download</button>
+          </div>
+        );
+
+      default:
+        return null;
+    }
+  };
+
+  return <div className="app">{renderScreen()}</div>;
+};
 
 export default App;
