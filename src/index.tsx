c25c7e59707717deb63f324f2f605363e9883a16
matan-h <matan.honig2@gmail.com>
2023-10-11 14:31:23 +0300

    commented out StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode> 
+  /* </React.StrictMode>  */
 );
 // NOTE: this React.StrictMode is what rendrer each component rendrer twice
 
